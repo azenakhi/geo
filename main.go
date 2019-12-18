ebdc235d83868b6272df2793c7c904e7a6204686
azenakhi <ahmed.zenakhi@gmail.com>
2019-12-18 17:50:47 +0100

    Update main.go

--- main.go
+++ main.go
@@ -2,8 +2,14 @@ package geo
 
 import (
 	"fmt"
+
+	"github.com/azenakhi/math/math"
+	mathv2 "github.com/azenakhi/math/v2/math"
 )
 
 func Toto() {
-	fmt.Println("m2")
+	m := math.Add(10, 10)
+	m2 := mathv2.Addionne(10, 10)
+	fmt.Println(m)
+	fmt.Println(m2)
 }
